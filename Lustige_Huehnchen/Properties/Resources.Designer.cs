4361bc4f8a95484b165ba361ff5af7341dff8fcd
Steven <36326543+Gurke258@users.noreply.github.com>
2025-05-29 19:16:26 +0200

    rename

--- Lustige_Huehnchen/Properties/Resources.Designer.cs
+++ Lustige_Huehnchen/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Lustige_Huehnchen.Properties {
+namespace Funny_Chicken.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Lustige_Huehnchen.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Lustige_Huehnchen.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Funny_Chicken.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
