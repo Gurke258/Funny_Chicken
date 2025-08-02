2c4de6bc8594e3452435edc9a0d76b96913a1848
Steven <36326543+Gurke258@users.noreply.github.com>
2025-08-02 19:46:24 +0200

    add class

---
+++ Lustige_Huehnchen/Database/SQLiteCon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Funny_Chicken.Database
+{
+    public class SQLiteCon
+    {
+        public void SQLLiteConnection(string query)
+        {
+            string connectionString = "Data Source=highscore.db;Version=3;";
+            SQLiteConnection connection = new SQLiteConnection(connectionString);
+            connection.Open();
+            SQLiteCommand command = new SQLiteCommand(query, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+    }
+}
